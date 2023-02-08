48605fc7eaeac0ffce4896672dd1846991d9f5c8
Teris <teristam@gmail.com>
2023-02-08 15:14:34 +0000

    Add plotly code

--- trialexp/process/pycontrol_utils.py
+++ trialexp/process/pycontrol_utils.py
@@ -7,8 +7,6 @@ import numpy as np
 
 def parse_events(session):
     #parse the event and state information and return it as a dataframe
-    # df_events = session.df_events
-    # df_conditions = session.conditions
 
     #parse the events list to distinguish between state and event
     state_names = session.state_IDs.keys()
@@ -60,7 +58,7 @@ def parse_events(session):
 
 
 def add_trial_number(df_events, trigger):
-    # trigger is a tuple containing the state and event_name e.g. ('waiting_for_spout','state')
+    # trigger is a tuple containing the state and event_name e.g. ('waiting_for_spout','state_change')
     
     df = df_events.copy()
 
@@ -97,16 +95,6 @@ def plot_session(df:pd.DataFrame, keys: list = None, state_def: list = None, pri
             eg. {'name':'trial', 'onset':'CS_Go', 'offset':'refrac_period'}
 
             For each onset, find the first offset event before the next onset 
-        
-        print_expr: list of dict #TODO need more testing
-            'name':'name of channel'
-            'expr': The expression '^\d+(?= ' + expr + ')' will be used for re.match()
-            list of regular expressions to be searched for self.print_lines and shown as an event channel
-
-            eg. {
-                'name':'water success',
-                'expr':'.?water success' # .? is needed if it is unknown whether there is any character ahead        
-            }
 
         event_ms: list of dict
                 'name':'name of something'
@@ -115,28 +103,8 @@ def plot_session(df:pd.DataFrame, keys: list = None, state_def: list = None, pri
 
         state_ms: list of dict #TODO
 
-        export_smrx: Bool = False
-            Save the plotted channels to a Spike 2 .smrx file.
-            An event channel and a state channel will be represetnted as an event and marker channels.
-            For the latter, onset and offset of a state is coded by 1 and 0 for code0.
-            Use
-                pip install sonpy
-            to install the sonpy module.
-
-            This metthod seems unstable. Tha same session data may fail ot succedd to export Spike2 file. Try restating kernel a few times. 
-            Apparently addition of time.sleep(0.05) helped to make this more stable.
-            Use verbose option to see what's going on.
-            When failed, the file size tends to be 11KB. Verbose will show [-1].
-            Restart the kernel to delete the corrupeted .smrx file.
-
-            Stylise the Spike2 display using notebooks|noncanonical|display_style.s2s
-
-        smrx_filename: str = None
-
         verbose :bool = False
 
-        print_to_text: bool = True
-            print_lines will be converted to text (and TextMark chaanel in Spike2)
 
         """
 
@@ -148,160 +116,11 @@ def plot_session(df:pd.DataFrame, keys: list = None, state_def: list = None, pri
         # 40 symbols
 
         fig = go.Figure()
-        # if keys is None:
-        #     keys = self.times.keys()
-        # else:
-        #     for k in keys: 
-        #        assert k in self.times.keys(), f"{k} is not found in self.time.keys()"
-
-        if export_smrx:
-            from sonpy import lib as sp
-            import time
-            if smrx_filename is None:
-                raise Exception('smrx_filename is required')
-            #TODO assert .smlx
-
-            mtc = re.search('\.smrx$', smrx_filename)
-            if mtc is None:
-                raise Exception('smrx_filename has to end with .smrx')
-
-            MyFile = sp.SonFile(smrx_filename)
-            CurChan = 0
-            UsedChans = 0
-            Scale = 65535/20
-            Offset = 0
-            ChanLow = 0
-            ChanHigh = 5
-            tFrom = 0
-            tUpto = sp.MaxTime64()         # The maximum allowed time in a 64-bit SON file
-            dTimeBase = 1e-6               # s = microseconds
-            x86BufSec = 2.
-            EventRate = 1/(dTimeBase*1e3)  # Hz, period is 1000 greater than the timebase
-            SubDvd = 1                     # How many ticks between attached items in WaveMarks
-
-            max_time_ms1 = np.max([np.max(self.times[k]) for k in keys if any(self.times[k])]) #TODO when no data 
-
-            list_of_match = [re.match('^\d+', L) for L in self.print_lines if re.match('^\d+', L) is not None]
-            max_time_ms2 = np.max([int(m.group(0)) for m in list_of_match])
-
-            max_time_ms = np.max([max_time_ms1, max_time_ms2])
-            time_vec_ms = np.arange(0, max_time_ms, 1000/EventRate)
-            # time_vec_micros = np.arange(0, max_time_ms*1000, 10**6 * 1/EventRate)
-
-            samples_per_s = EventRate
-            interval = 1/samples_per_s
-
-            samples_per_ms = 1/1000 * EventRate
-            interval = 1/samples_per_s
-
-            MyFile.SetTimeBase(dTimeBase)  # Set timebase
-
-
-        def write_event(MyFile, X_ms, title, y_index, EventRate, time_vec_ms):
-            (hist, ___) = np.histogram(X_ms, bins=time_vec_ms) # time is 1000 too small
-
-            eventfalldata = np.where(hist)
-
-            MyFile.SetEventChannel(y_index, EventRate)
-            MyFile.SetChannelTitle(y_index, title)
-            if eventfalldata[0] is not []:
-                MyFile.WriteEvents(int(y_index), eventfalldata[0]*1000) #dirty fix but works
-                time.sleep(0.05)# might help?
-
-            if verbose:
-                print(f'{y_index}, {title}:')
-                nMax = 10
-                # nMax = int(MyFile.ChannelMaxTime(int(y_index))/MyFile.ChannelDivide(int(y_index))) 
-                print(MyFile.ReadEvents(int(y_index), nMax, tFrom, tUpto)) #TODO incompatible function arguments.
-                # [-1] when failed
-
-                # ReadEvents(self: sonpy.amd64.sonpy.SonFile, 
-                #     chan: int, 
-                #     nMax: int, # probably the end of the range to read in the unit of number of channel divide
-                #     tFrom: int, 
-                #     tUpto: int = 8070450532247928832, 
-                #     Filter: sonpy.amd64.sonpy.MarkerFilter = <sonpy.MarkerFilter> in mode 'First', with trace column -1 and items
-                #     Layer 1 [
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-                #     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-
-        def write_marker_for_state(MyFile,X_ms, title, y_index, EventRate, time_vec_ms):
-            #TODO nearly there, but file cannot be open
-
-            # remove NaN
-            X_notnan_ms = [x for x in X_ms if not np.isnan(x)]
-
-            (hist, ___) = np.histogram(X_notnan_ms, bins=time_vec_ms) # time is 1000 too small
-
-            eventfalldata = np.where(hist)
-
-            nEvents = len(eventfalldata[0])
-
-            MarkData = np.empty(nEvents, dtype=sp.DigMark)
-            for i in range(nEvents):
-                if (i+1) % 2 == 0:
-                    MarkData[i] = sp.DigMark(eventfalldata[0][i]*1000, 0) #offset
-                elif (i+1) % 2 == 1:
-                    MarkData[i] = sp.DigMark(eventfalldata[0][i]*1000, 1) #onset
-                else:
-                    raise Exception('oh no')
-            MyFile.SetMarkerChannel(y_index, EventRate)
-            MyFile.SetChannelTitle(y_index, title)
-            if eventfalldata[0] is not []:
-                MyFile.WriteMarkers(int(y_index), MarkData)
-                time.sleep(0.05)# might help?
-
-            if verbose:             
-                print(f'{y_index}, {title}:')
-                print(MyFile.ReadMarkers(int(y_index), nEvents, tFrom, tUpto)) #TODO failed Tick = -1
-
-        def write_textmark(MyFile, X_ms, title, y_index, txt, EventRate, time_vec_ms):
-
-            (hist, ___) = np.histogram(X_ms, bins=time_vec_ms) # time is 1000 too small
-
-            eventfalldata = np.where(hist)
-
-            nEvents = len(eventfalldata[0])
-
-            MarkData = np.empty(nEvents, dtype=sp.DigMark)
-
-            TMrkData = np.empty(nEvents, dtype=sp.TextMarker)
-
-            for i in range(nEvents):
-                if (i+1) % 2 == 0:
-                    MarkData[i] = sp.DigMark(eventfalldata[0][i]*1000, 0) #offset
-                elif (i+1) % 2 == 1:
-                    MarkData[i] = sp.DigMark(eventfalldata[0][i]*1000, 1) #onset
-                else:
-                    raise Exception('oh no')
-                TMrkData[i] = sp.TextMarker(txt[i], MarkData[i]) #TODO
-                
-            MyFile.SetTextMarkChannel(y_index, EventRate, max(len(s) for s in txt)+1)
-            MyFile.SetChannelTitle(y_index, title)
-            if eventfalldata[0] is not []:
-                MyFile.WriteTextMarks(y_index, TMrkData)
-                time.sleep(0.05)# might help?
-
-            if verbose:
-                print(f'{y_index}, {title}:')
-                try:
-                    print(MyFile.ReadTextMarks(int(y_index), nEvents, tFrom, tUpto))
-                except:
-                    print('error in print')
+        if keys is None:
+            keys = df.event_name.unique()
+        else:
+            for k in keys: 
+               assert k in df.event_name.unique(), f"{k} is not found in self.time.keys()"
 
         def find_states(state_def_dict: dict):
             """
@@ -324,8 +143,8 @@ def plot_session(df:pd.DataFrame, keys: list = None, state_def: list = None, pri
             if state_def_dict is None:
                 return None
 
-            all_on_ms = self.times[state_def_dict['onset']]
-            all_off_ms = self.times[state_def_dict['offset']]
+            all_on_ms = df[(df.state == state_def_dict['onset']) & (df.event_name == 'state_change')].time.values
+            all_off_ms = df[(df.state == state_def_dict['offset']) & (df.event_name == 'state_change')].time.values
 
             onsets_ms = [np.NaN] * len(all_on_ms)
             offsets_ms = [np.NaN] * len(all_on_ms)
@@ -353,6 +172,7 @@ def plot_session(df:pd.DataFrame, keys: list = None, state_def: list = None, pri
                            [np.NaN] * len(onsets_ms)))
             # [onset1, offset1, NaN, onset2, offset2, NaN, ....]
             state_ms = [item for sublist in state_ms for item in sublist]
+            
             return state_ms
 
         y_index = 0
@@ -368,93 +188,45 @@ def plot_session(df:pd.DataFrame, keys: list = None, state_def: list = None, pri
                 write_event(MyFile, df.time[k], k, y_index, EventRate, time_vec_ms)
 
 
-
-        # if print_expr is not None: #TODO
-        #     if isinstance(print_expr, dict):
-        #         print_expr = [print_expr]
-
-        #     for dct in print_expr:
-        #         y_index += 1
-        #         expr = '^\d+(?= ' + dct['expr'] + ')'
-        #         list_of_match = [re.match(expr, L) for L in self.print_lines if re.match(expr, L) is not None]
-        #         ts_ms = [int(m.group(0)) for m in list_of_match]
-        #         line2 = go.Scatter(
-        #             x=[TS_ms/1000 for TS_ms in ts_ms], y=[dct['name']] * len(ts_ms), 
-        #             name=dct['name'], mode='markers', marker_symbol=symbols[y_index % 40])
-        #         fig.add_trace(line2)
-
-        #         if export_smrx:
-        #             write_event(
-        #                 MyFile, ts_ms, dct['name'], y_index, EventRate, time_vec_ms)
-
-        # if event_ms is not None:
-        #     if isinstance(event_ms, dict):
-        #         event_ms = [event_ms]
+        if event_ms is not None:
+            if isinstance(event_ms, dict):
+                event_ms = [event_ms]
             
-        #     for dct in event_ms:
-        #         y_index += 1
-        #         line3 = go.Scatter(
-        #             x=[t/1000 for t in dct['time_ms']],
-        #             y=[dct['name']] * len(dct['time_ms']),
-        #             name=dct['name'], mode='markers', marker_symbol=symbols[y_index % 40])
-        #         fig.add_trace(line3)
+            for dct in event_ms:
+                y_index += 1
+                line3 = go.Scatter(
+                    x=[t/1000 for t in dct['time_ms']],
+                    y=[dct['name']] * len(dct['time_ms']),
+                    name=dct['name'], mode='markers', marker_symbol=symbols[y_index % 40])
+                fig.add_trace(line3)
 
-        #         if export_smrx:
-        #             write_event(
-        #                 MyFile, dct['time_ms'], dct['name'], y_index, EventRate, time_vec_ms)
 
-        # if print_to_text:
+        if state_def is not None:
+            # Draw states as gapped lines
+            # Assuming a list of lists of two names
 
-        #     EXPR = '^(\d+)\s(.+)'
-        #     list_of_match = [re.match(EXPR, L) for L in self.print_lines if re.match(EXPR, L) is not None]
-        #     ts_ms = [int(m.group(1)) for m in list_of_match]
-        #     txt = [m.group(2) for m in list_of_match]
-  
-        #     df_print = pd.DataFrame(list(zip(ts_ms, txt)), columns=['ms', 'text'])
-
-        #     y_index += 1
-        #     txtsc = go.Scatter(x=[TS_ms/1000 for TS_ms in ts_ms], y=['print_lines']*len(ts_ms), 
-        #         text=txt, textposition="top center", 
-        #         mode="markers", marker_symbol=symbols[y_index % 40])
-        #     fig.add_trace(txtsc)
-
-        #     if export_smrx:
-        #         write_textmark( MyFile, ts_ms, 'print lines', y_index, txt, EventRate, time_vec_ms)
-
-        # if state_def is not None:
-        #     # Draw states as gapped lines
-        #     # Assuming a list of lists of two names
-
-        #     if isinstance(state_def, dict):# single entry
-        #         state_def = [state_def]
-        #         # state_ms = find_states(state_def)
-
-        #         # line1 = go.Scatter(x=[x/1000 for x in state_ms], y=[state_def['name']] * len(state_ms), 
-        #         #     name=state_def['name'], mode='lines', line=dict(width=5))
-        #         # fig.add_trace(line1)
-
-        #     if isinstance(state_def, list):# multiple entry
-        #         state_ms = None
-        #         for i in state_def:
-        #             assert isinstance(i, dict)
+            if isinstance(state_def, list):# multiple entry
+                state_ms = None
+                for state in state_def:
+                    assert isinstance(state, dict)
                     
-        #             y_index +=1
-        #             state_ms = find_states(i)
+                    y_index +=1
+                    state_ms = find_states(state)
+                    print(state['name'], state_ms)
+                    print([state['name']] * len(state_ms))
 
-        #             line1 = go.Scatter(x=[x/1000 for x in state_ms], y=[i['name']] * len(state_ms), 
-        #                 name=i['name'], mode='lines', line=dict(width=5))
-        #             fig.add_trace(line1)
+                    line1 = go.Scatter(x=[x/1000 for x in state_ms], y=[state['name']] * len(state_ms), 
+                        name=state['name'], mode='lines', line=dict(width=5))
+                    fig.add_trace(line1)
 
-        #             if export_smrx:
-        #                 write_marker_for_state(MyFile, state_ms, i['name'], y_index, EventRate, time_vec_ms)
-        #     else:
-        #         state_ms = None
-        # else:
-        #     state_ms = None
+            else:
+                state_ms = None
+        else:
+            state_ms = None
              
 
         fig.update_xaxes(title='Time (s)')
-        fig.update_yaxes(fixedrange=True) # Fix the Y axis
+        # fig.update_yaxes(fixedrange=True) # Fix the Y axis
 
         # fig.update_layout(
             
