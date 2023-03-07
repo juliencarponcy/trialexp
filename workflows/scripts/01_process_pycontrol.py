109caccf61f76a2b9aa5cb9fac3ea4e168241971
Teris <teristam@gmail.com>
2023-03-07 14:03:51 +0000

    [pycontrol] save raw pycontrol dataframe

--- workflows/scripts/01_process_pycontrol.py
+++ workflows/scripts/01_process_pycontrol.py
@@ -21,9 +21,9 @@ except ModuleNotFoundError:
 #%% Load inputs
 
 (sinput, soutput) = getSnake(locals(), 'workflows/spout_bar_nov22.smk',
-  ['Z:/Teris/ASAP/expt_sessions/JC318L-2022-11-30-175356/processed/df_events_cond.pkl'],
+#  ['Z:/Julien/Data/head-fixed/_Other/test_folder/by_session_folder/JC317L-2022-12-16-174417\processed/df_events_cond.pkl'],
+  ['Z:/Teris/ASAP/expt_sessions/kms064-2023-02-08-100449/processed/df_events_cond.pkl'],
   'process_pycontrol')
-
 
 #%% Read pycontrol file
 
@@ -33,10 +33,11 @@ if len(filename)>1:
 
 df_session = di.session_dataframe(filename[0])
 df_pycontrol = parse_session_dataframe(df_session)
-
 session_time = datetime.strptime(df_pycontrol.attrs['Start date'], '%Y/%m/%d %H:%M:%S')
 subjectID = df_pycontrol.attrs['Subject ID']
 task_name = df_pycontrol.attrs['Task name']
+
+df_pycontrol.to_pickle(soutput.pycontrol_dataframe)
 
 #%% Read task definition
 tasks = pd.read_csv('params/tasks_params.csv', usecols=[1, 2, 3, 4], index_col=False)
@@ -47,7 +48,7 @@ timelim = [1000, 4000] # in ms
 conditions, triggers, events_to_process = get_task_specs(tasks,  task_name)
 
 #%% Extract trial-related information from events
-
+df_pycontrol = df_pycontrol[~(df_pycontrol.name=='rsync')] #remove the sync pulse
 df_pycontrol  = print2event(df_pycontrol, conditions)
 
 df_events_trials, df_events = extract_trial_by_trigger(df_pycontrol, triggers[0], conditions+events_to_process, 
@@ -74,5 +75,6 @@ df_events_cond.attrs.update(
 
 df_events_cond.to_pickle(soutput.event_dataframe)
 df_conditions.to_pickle(soutput.condition_dataframe)
+df_events_trials.to_pickle(soutput.trial_dataframe)
 
 # %%
