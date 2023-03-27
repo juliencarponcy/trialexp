e3c3aff8c8ffc0d85cd6d81e759e1fa013ffec27
Teris <teristam@gmail.com>
2023-03-27 16:39:11 +0100

    [session folder] Match pyphotometry file with pycontrol file and add more checks

--- workflows/scripts/00_create_session_folders.py
+++ workflows/scripts/00_create_session_folders.py
@@ -2,15 +2,22 @@
 Script to create the session folder structure
 '''
 #%%
-from pathlib import Path
-from glob import glob
 import os
-import shutil
-from tqdm.auto import tqdm
-from datetime import datetime. Tim
 import re
+import shutil
+import warnings
+from datetime import datetime, timedelta
+from glob import glob
+from pathlib import Path
+
+import numpy as np
 import pandas as pd
-import numpy as np 
+from tqdm.auto import tqdm
+
+from trialexp.process.pycontrol.data_import import session_dataframe
+from trialexp.process.pyphotometry.utils import import_ppd
+from trialexp.utils.rsync import Rsync_aligner, RsyncError
+
 #%% 
 export_base_path = Path('Z:\Teris\ASAP\expt_sessions')
 pycontrol_folder = Path('Z:\\Teris\\ASAP\\pycontrol\\reaching_go_spout_bar_nov22')
@@ -29,7 +36,9 @@ def parse_pyhoto_fn(fn):
         date_string = m.group(2)
         expt_datetime = datetime.strptime(date_string, "%Y-%m-%d-%H%M%S")
         
-        return {'animal_id':animal_id, 'path':fn, 'filename':fn.stem, 'timestamp':expt_datetime}
+        return {'animal_id':animal_id, 'path':fn, 
+                'filename':fn.stem, 
+                'timestamp':expt_datetime}
 
 df_pyphoto = pd.DataFrame(list(map(parse_pyhoto_fn, pyphoto_files)))
 
@@ -42,7 +51,10 @@ def parse_pycontrol_fn(fn):
         date_string = m.group(2)
         expt_datetime = datetime.strptime(date_string, "%Y-%m-%d-%H%M%S")
         
-        return {'animal_id':animal_id, 'path':fn, 'filename':fn.stem, 'timestamp':expt_datetime}
+        return {'animal_id':animal_id, 'path':fn,                 
+                'session_id':fn.stem,
+                'filename':fn.stem, 
+                'timestamp':expt_datetime}
     
 df_pycontrol = pd.DataFrame(list(map(parse_pycontrol_fn, pycontrol_files)))
 
@@ -65,34 +77,58 @@ for _, row in df_pycontrol.iterrows():
 
 df_pycontrol['pyphoto_path'] = matched_path
 df_pycontrol['pyphoto_filename'] = matched_fn
+df_pycontrol = df_pycontrol[df_pycontrol.animal_id!='00'] # do not copy the test data
 df_pycontrol = df_pycontrol.dropna(subset='pyphoto_path')
+
 # %%
-for fn in tqdm(pycontrol_files):
-    session_id = fn.stem
-    animal_id = session_id.split('-')[0]
-    
-    if not animal_id=='00': # do not copy the test data
-        target_pycontrol_folder = Path(export_base_path, session_id, 'pycontrol')
-        target_pyphoto_folder = Path(export_base_path, session_id, 'pyphotometry')
+def create_sync(pycontrol_file, pyphotometry_file):
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        pyphotometry_file = import_ppd(pyphotometry_file)
+        data_pycontrol = session_dataframe(pycontrol_file)
+
+        photo_rsync = pyphotometry_file['pulse_times_2']
+        pycontrol_rsync = data_pycontrol[data_pycontrol.name=='rsync'].time
         
-        if not target_pycontrol_folder.exists():
-            # create the base folder
-            target_pycontrol_folder.mkdir(parents=True)
-            target_pyphoto_folder.mkdir(parents=True)
+        try:
+            return Rsync_aligner(pulse_times_A= photo_rsync, 
+            pulse_times_B= pycontrol_rsync, plot=False) #align pycontrol time to pyphotometry time
+        except (RsyncError, ValueError):
+            return None
+
+# %%
+matched = []
+for _, row in df_pycontrol.iterrows():
+    session_id = row.session_id
+    animal_id = row.animal_id
+    
+    target_pycontrol_folder = Path(export_base_path, session_id, 'pycontrol')
+    target_pyphoto_folder = Path(export_base_path, session_id, 'pyphotometry')
+    
+    if not target_pycontrol_folder.exists():
+        # create the base folder
+        target_pycontrol_folder.mkdir(parents=True)
+        target_pyphoto_folder.mkdir(parents=True)
+        
+        pycontrol_file = row.path
+        pyphotometry_file = row.pyphoto_path
+        
+        #double check they are really of the same session
+        if create_sync(str(pycontrol_file), str(pyphotometry_file)) is not None:
+            matched.append(True)
             
-            
-            shutil.copy(fn, target_pycontrol_folder)
+            #copy the files
+            shutil.copy(pycontrol_file, target_pycontrol_folder)
+            shutil.copy(pyphotometry_file, target_pyphoto_folder)
             
             #copy all the analog data
-            analog_files = fn.parent.glob(f'{session_id}*.pca')
+            analog_files = pycontrol_file.parent.glob(f'{session_id}*.pca')
             for f in analog_files:
-                shutil.copy(f, target_pycontrol_folder)
-                
-                
-            # Next work on matching the pyphotometry folder
-            
+                shutil.copy(f, target_pycontrol_folder)                
         else:
-            print(f'{session_id} already exist')
-        
+            matched.append(False)
 
+    else:
+        print(f'{session_id} already exist')
+df_pycontrol['matched'] = matched
 # %%
