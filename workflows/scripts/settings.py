5b49d15a801f6ea5b8db7d5151dfd906150d0f44
juliencarponcy <41296546+juliencarponcy@users.noreply.github.com>
2023-04-04 14:24:02 +0100

    s00_create_sorted_folders for folder preparation

--- workflows/scripts/settings.py
+++ workflows/scripts/settings.py
@@ -1,4 +1,4 @@
 # debug_folder = r'//ettin/Magill_Lab/Teris/ASAP/expt_sessions/kms063-2023-02-27-164426/'
-debug_folder = r'//ettin/Magill_Lab/Teris/ASAP/expt_sessions/kms058-2023-03-07-104332/'
+debug_folder = r'//home/MRC.OX.AC.UK/phar0732/ettin/Data/head-fixed/_Other/test_folder_ephys/kms058_2023-03-24_15-09-44_bar_final/Record Node 101/experiment1/recording1/continuous/Neuropix-PXI-100.ProbeA-AP/sorting/'
 
 # debug_folder = r'//ettin/Magill_Lab/Julien/Data/head-fixed/_Other/test_folder/by_session_folder/JC317L-2022-12-16-173145'
