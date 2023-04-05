59b8c60d9b72d2b6d6ab0a2986fca80963e38c05
Teris <teristam@gmail.com>
2023-04-05 12:51:49 +0100

    [refactor] move analysis parameter to a central settings file [bugfix] supply timelim to compute_success to avoid error

--- workflows/scripts/01_process_pycontrol.py
+++ workflows/scripts/01_process_pycontrol.py
@@ -37,9 +37,8 @@ df_pycontrol.to_pickle(soutput.pycontrol_dataframe)
 
 #%% Read task definition
 tasks = pd.read_csv('params/tasks_params.csv', usecols=[1, 2, 3, 4], index_col=False)
-
-trial_window = [-2000, 4000]
-timelim = [1000, 4000] # in ms
+trial_window = settings.trial_window
+timelim = settings.timelim
 
 conditions, triggers, events_to_process = get_task_specs(tasks,  task_name)
 
@@ -53,7 +52,7 @@ df_events_trials, df_events = extract_trial_by_trigger(df_pycontrol, triggers[0]
 
 df_conditions = compute_conditions_by_trial(df_events_trials, conditions)
 
-df_conditions = compute_success(df_events_trials, df_conditions, task_name)
+df_conditions = compute_success(df_events_trials, df_conditions, task_name, timelim =timelim)
 
 #%%  Merge condition back with event dataframe
 
--- workflows/scripts/settings.py
+++ workflows/scripts/settings.py
@@ -1,4 +1,13 @@
-# debug_folder = r'//ettin/Magill_Lab/Teris/ASAP/expt_sessions/kms063-2023-02-27-164426/'
-debug_folder = r'//ettin/Magill_Lab/Teris/ASAP/expt_sessions/kms058-2023-03-07-104332/'
+######################
+## Analysis settings
 
-# debug_folder = r'//ettin/Magill_Lab/Julien/Data/head-fixed/_Other/test_folder/by_session_folder/JC317L-2022-12-16-173145'
+trial_window = [-2000, 4000]
+timelim = [1000, 4000] # in ms
+
+#################
+## Debug settings
+
+# debug_folder = r'//ettin/Magill_Lab/Teris/ASAP/expt_sessions/kms063-2023-02-27-164426/'
+# debug_folder = r'//ettin/Magill_Lab/Teris/ASAP/expt_sessions/kms058-2023-03-07-104332/'
+
+debug_folder = r'//ettin/Magill_Lab/Julien/Data/head-fixed/by_sessions/cued_uncued_oct22/kms047-2022-10-10-155638/'
