be4c60531e4015aca9f4169dfb2961501bc252d5
juliencarponcy <juliencarponcy@gmail.com>
2023-04-12 15:17:55 +0100

    sorting pipeline basic run

--- workflows/scripts/s01_sort_ks3.py
+++ workflows/scripts/s01_sort_ks3.py
@@ -22,15 +22,16 @@ from trialexp.process.ephys.spikesort import sort
 #%% Load inputs
 
 (sinput, soutput) = getSnake(locals(), 'workflows/spikesort.smk',
-  [settings.debug_folder + '/processed/spike_sorting.done'],
+  [settings.debug_folder + r'/processed/spike_sorting.done'],
   'spike_sorting')
 
 
 # %%
-session_path = '/home/MRC.OX.AC.UK/phar0732/ettin/Data/head-fixed/by_sessions/reaching_go_spout_bar_nov22/kms058-2023-03-25-184034'
-# rec_path = 
+# session_path = '/home/MRC.OX.AC.UK/phar0732/ettin/Data/head-fixed/by_sessions/reaching_go_spout_bar_nov22/kms058-2023-03-25-184034'
+# # rec_path = 
 sorter_name = 'kilosort3'
-rec_properties_path = Path(session_path) / 'ephys' / 'rec_properties.csv'
+# rec_properties_path = Path(session_path) / 'ephys' / 'rec_properties.csv'
+rec_properties_path = Path(sinput.rec_properties)
 rec_properties = pd.read_csv(rec_properties_path, index_col= None)
 
 output_folder = rec_properties_path.parent / 'sorting'
@@ -38,6 +39,7 @@ output_folder = rec_properties_path.parent / 'sorting'
 # Only select longest syncable recordings to sort
 idx_to_sort = rec_properties[rec_properties.longest == True].index.values
 
+root_data_path = r'\\ettin\Magill_Lab\Julien'
 
 # %%
 for idx_rec in idx_to_sort:
@@ -45,7 +47,9 @@ for idx_rec in idx_to_sort:
     rec_nb = rec_properties.rec_nb.iloc[idx_rec]
     AP_stream = rec_properties.AP_stream.iloc[idx_rec]
     ephys_path = Path(rec_properties.full_path.iloc[idx_rec]).parent.parent.parent.parent.parent
-
+    relative_ephys_path = os.path.join(*ephys_path.parts[5:])
+    ephys_path = os.path.join(root_data_path, relative_ephys_path)
+    
     recordings = se.read_openephys(ephys_path, block_index=exp_nb-1, stream_name=AP_stream)
     recording = select_segment_recording(recordings, segment_indices= int(rec_nb-1))
 
