6d6c26be2e369dcb9627d3ad4fb06ab31e09f5e0
juliencarponcy <41296546+juliencarponcy@users.noreply.github.com>
2023-04-12 18:06:29 +0100

    adding sorter_specific_params, jade version

--- workflows/scripts/s01_sort_ks3.py
+++ workflows/scripts/s01_sort_ks3.py
@@ -39,13 +39,23 @@ output_folder = rec_properties_path.parent / 'sorting'
 # Only select longest syncable recordings to sort
 idx_to_sort = rec_properties[rec_properties.longest == True].index.values
 
-root_data_path = r'\\ettin\Magill_Lab\Julien'
+root_data_path = r'/home/MRC.OX.AC.UK/phar0732/ettin/'
 
 # %%
 for idx_rec in idx_to_sort:
     exp_nb = rec_properties.exp_nb.iloc[idx_rec]
     rec_nb = rec_properties.rec_nb.iloc[idx_rec]
     AP_stream = rec_properties.AP_stream.iloc[idx_rec]
+    
+    # symplifying folder names for each probe
+    if 'ProbeA' in AP_stream:    
+        probe_folder_name = 'ProbeA'
+    elif 'ProbeB' in AP_stream:
+        probe_folder_name = 'ProbeB'
+    else:
+        raise ValueError(f'invalid probe name rec: {rec_properties_path.parent}')
+    
+    
     ephys_path = Path(rec_properties.full_path.iloc[idx_rec]).parent.parent.parent.parent.parent
     relative_ephys_path = os.path.join(*ephys_path.parts[5:])
     ephys_path = os.path.join(root_data_path, relative_ephys_path)
@@ -53,12 +63,22 @@ for idx_rec in idx_to_sort:
     recordings = se.read_openephys(ephys_path, block_index=exp_nb-1, stream_name=AP_stream)
     recording = select_segment_recording(recordings, segment_indices= int(rec_nb-1))
 
+    sorter_specific_params = {
+        # 'n_jobs': 24, 
+        # 'total_memory': 512000000000, 
+        # 'chunk_size': None, 
+        # 'chunk_memory': 12800000000,
+        'chunk_duration': '10s', 
+        'progress_bar': True}
+
     sorting = ss.run_sorter(
             sorter_name = sorter_name,
             recording = recording, 
-            output_folder = output_folder / AP_stream,
+            output_folder = output_folder / probe_folder_name,
             remove_existing_folder = True, 
             delete_output_folder = False, 
-            verbose = True)
+            verbose = True,
+            **sorter_specific_params)
 
+    sorting.save(folder= output_folder / probe_folder_name)
 # %%
--- workflows/spikesort.smk
+++ workflows/spikesort.smk
@@ -20,7 +20,7 @@ rule spike_sorting:
     input:
         rec_properties = '{session_path}/{task_path}/{session_id}/ephys/rec_properties.csv'
     output:
-        spike_templateA = '{session_path}/{task_path}/{session_id}/ephys/probeA/spike_templates.npy',
-        spike_templateB = '{session_path}/{task_path}/{session_id}/ephys/probeB/spike_templates.npy',
+        spike_templateA = '{session_path}/{task_path}/{session_id}/ephys/sorting/probeA/spike_templates.npy',
+        spike_templateB = '{session_path}/{task_path}/{session_id}/ephys/sorting/probeB/spike_templates.npy',
         rule_complete = touch(r'{session_path}/{task_path}/{session_id}/processed/spike_sorting.done')
 
