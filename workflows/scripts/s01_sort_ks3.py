e62bb1619b0fca6233496edae925ad4a7d60b951
juliencarponcy <41296546+juliencarponcy@users.noreply.github.com>
2023-04-13 11:51:30 +0100

    adjust to ephys with one vs several experiments

--- workflows/scripts/s01_sort_ks3.py
+++ workflows/scripts/s01_sort_ks3.py
@@ -30,6 +30,7 @@ from trialexp.process.ephys.spikesort import sort
 # session_path = '/home/MRC.OX.AC.UK/phar0732/ettin/Data/head-fixed/by_sessions/reaching_go_spout_bar_nov22/kms058-2023-03-25-184034'
 # # rec_path = 
 sorter_name = 'kilosort3'
+verbose = True
 # rec_properties_path = Path(session_path) / 'ephys' / 'rec_properties.csv'
 rec_properties_path = Path(sinput.rec_properties)
 rec_properties = pd.read_csv(rec_properties_path, index_col= None)
@@ -55,13 +56,24 @@ for idx_rec in idx_to_sort:
     else:
         raise ValueError(f'invalid probe name rec: {rec_properties_path.parent}')
     
-    
+    output_probe_folder = output_folder / probe_folder_name 
+    if not output_probe_folder.exists():
+        output_probe_folder.mkdir(parents=True)
+
     ephys_path = Path(rec_properties.full_path.iloc[idx_rec]).parent.parent.parent.parent.parent
     relative_ephys_path = os.path.join(*ephys_path.parts[5:])
     ephys_path = os.path.join(root_data_path, relative_ephys_path)
     
-    recordings = se.read_openephys(ephys_path, block_index=exp_nb-1, stream_name=AP_stream)
-    recording = select_segment_recording(recordings, segment_indices= int(rec_nb-1))
+    experiments_nb = rec_properties.exp_nb.unique()
+    if len(experiments_nb) == 1:
+        recordings = se.read_openephys(ephys_path, block_index=exp_nb-1, stream_name=AP_stream) # nb-based
+    else:
+        recordings = se.read_openephys(ephys_path, block_index=exp_nb, stream_name=AP_stream) # nb-based
+    
+    recording = select_segment_recording(recordings, segment_indices= int(rec_nb-1)) # index-based
+
+    if verbose:
+        print(f'{Path(ephys_path).parts[-1]}, {probe_folder_name}, exp nb:{exp_nb} rec nb:{rec_nb}. recording duration: {recording.get_total_duration()}')   
 
     sorter_specific_params = {
         # 'n_jobs': 24, 
@@ -74,7 +86,7 @@ for idx_rec in idx_to_sort:
     sorting = ss.run_sorter(
             sorter_name = sorter_name,
             recording = recording, 
-            output_folder = output_folder / probe_folder_name,
+            output_folder = output_probe_folder,
             remove_existing_folder = True, 
             delete_output_folder = False, 
             verbose = True,
--- workflows/spikesort.smk
+++ workflows/spikesort.smk
@@ -15,12 +15,28 @@ rule all:
 #     script:
 #         'scripts/s00_create_session_folders.py'
 
+def rec_properties_input(wildcards):
+    # determine if there is an ephys recording for that folder
+    recording_csv = glob(f'{wildcards.session_path}/{wildcards.task_path}/{wildcards.session_id}/ephys/rec_properties.csv')
+    if len(recording_csv) > 0:
+        return f'{wildcards.session_path}/{wildcards.task_path}/{wildcards.session_id}/ephys/rec_properties.csv'
+    else:
+        return []
 
 rule spike_sorting:
     input:
-        rec_properties = '{session_path}/{task_path}/{session_id}/ephys/rec_properties.csv'
+        rec_properties = rec_properties_input
     output:
         spike_templateA = '{session_path}/{task_path}/{session_id}/ephys/sorting/probeA/spike_templates.npy',
         spike_templateB = '{session_path}/{task_path}/{session_id}/ephys/sorting/probeB/spike_templates.npy',
         rule_complete = touch(r'{session_path}/{task_path}/{session_id}/processed/spike_sorting.done')
+    log:
+        '{session_path}/{task_path}/{session_id}/processed/log/process_spike_sorting.log'
+    script:
+        'scripts/s01_sort_ks3.py'
 
+rule final:
+    input:
+        spike_sorting_done = '{session_path}/{task_path}/{session_id}/processed/spike_sorting.done'
+    output:
+        done = touch('{session_path}/{task_path}/{session_id}/processed/spike_workflow.done')
