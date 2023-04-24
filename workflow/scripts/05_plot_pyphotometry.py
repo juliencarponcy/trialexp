f973433d4d39b5b0ca91858709e668510caa71dd
Teris Tam <teristam@gmail.com>
2023-04-24 16:01:13 +0100

    [bugfix] check if plotting can be done properly first

--- workflow/scripts/05_plot_pyphotometry.py
+++ workflow/scripts/05_plot_pyphotometry.py
@@ -33,11 +33,14 @@ for k in xr_session.data_vars.keys():
     if 'event_time' in da.coords:
         df2plot = xr_session[[k,'success']].to_dataframe().reset_index()
         
-        fig, ax = plt.subplots(1,1,dpi=300, figsize=(6,6))
+        if not all(df2plot[k].isna()): #make sure data are correct
+          
+          fig, ax = plt.subplots(1,1,dpi=300, figsize=(6,6))
 
-        ax = sns.lineplot(x='event_time',hue='success', y=k, data=df2plot)
-        ax.set(ylabel=k, xlabel='Delta F/F')
+          ax = sns.lineplot(x='event_time',hue='success', y=k, data=df2plot)
+          ax.set(ylabel=k, xlabel='Delta F/F')
 
-        fig.savefig(os.path.join(figure_dir, f'{k}.png'), dpi=300, bbox_inches='tight')
+          fig.savefig(os.path.join(figure_dir, f'{k}.png'), dpi=300, bbox_inches='tight')
 
 xr_session.close()
+# %%
