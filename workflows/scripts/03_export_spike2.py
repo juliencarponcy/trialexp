576e1cc7cc76b4af013c61e4b78d71b3925a78e3
Kouichi C. Nakamura <kouichi.c.nakamura@gmail.com>
2023-04-25 09:34:27 +0100

    03

--- workflows/scripts/03_export_spike2.py
+++ workflows/scripts/03_export_spike2.py
@@ -6,6 +6,8 @@ import pandas as pd
 from trialexp.process.pycontrol.utils import export_session
 from snakehelper.SnakeIOHelper import getSnake
 from workflows.scripts import settings
+from glob import glob 
+from trialexp.process.pyphotometry.utils import *
 
 #%%
 
@@ -13,8 +15,18 @@ from workflows.scripts import settings
     [settings.debug_folder +'\processed\spike2.smrx'],
   'export_spike2')
 
+#%% Photometry dict
+
+fn = glob(sinput.photometry_folder+'\*.ppd')[0]
+data_photometry = import_ppd(fn)
+data_photometry = denoise_filter(data_photometry)
+data_photometry = motion_correction(data_photometry)
+data_photometry = compute_df_over_f(data_photometry, low_pass_cutoff=0.001)
+
+
 #%% Load data
 df_pycontrol = pd.read_pickle(sinput.pycontrol_dataframe)
+
 
 #remove all state change event
 df_pycontrol = df_pycontrol.dropna(subset='name')
@@ -24,3 +36,5 @@ keys = df2plot.name.unique()
 export_session(df_pycontrol, keys,
              smrx_filename=soutput.spike2_file)
 # %%
+import xarray as xr
+xr_photo = xr.load_dataset(sinput.df_photometry)
