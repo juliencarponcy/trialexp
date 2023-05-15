fab659fef5b69f599fefdc2ce3f1fe8af0164269
Kouichi C. Nakamura <kouichi.c.nakamura@gmail.com>
2023-05-15 11:46:30 +0100

    export successful

--- notebooks/noncanonical/nb20230512_175700_eval_motion_correction_win.py
+++ notebooks/noncanonical/nb20230512_175700_eval_motion_correction_win.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[10]:
 
 
 import os
@@ -29,7 +29,7 @@ task1 = join_task_session('reaching_go_spout_bar_nov22', [
 debug_folders = task1
 
 
-# In[3]:
+# In[32]:
 
 
 import pandas as pd
@@ -47,6 +47,9 @@ class sinput_class():
         self.pycontrol_dataframe = None
         self.pycontrol_folder = None
 
+class soutput_class():
+    def __init__(self):
+        self.spike2_file = None
 
 def load_and_prep_photom(debug_folder):
     # (sinput, soutput) = getSnake(locals(), 'workflow/pycontrol.smk',
@@ -57,6 +60,10 @@ def load_and_prep_photom(debug_folder):
     sinput.pycontrol_dataframe = os.path.join(
         debug_folder, 'processed', 'df_pycontrol.pkl')
     sinput.pycontrol_folder = os.path.join(debug_folder, 'pycontrol')
+
+    soutput = soutput_class()
+    soutput.spike2_file = os.path.join(
+        debug_folder, 'processed', 'spike2C.smrx')
 
     # %% Photometry dict
 
@@ -122,7 +129,7 @@ def load_and_prep_photom(debug_folder):
                        'analog_1_est_motion', 'analog_1_corrected', 'analog_1_baseline_fluo',
                        'analog_1_df_over_f']
 
-    return df_pycontrol, pycontrol_time, data_photometry, photometry_times_pyc, photometry_keys
+    return df_pycontrol, pycontrol_time, data_photometry, photometry_times_pyc, photometry_keys, sinput, soutput
 
 # export_session(df_pycontrol, keys,
 #     data_photometry = data_photometry,
@@ -133,17 +140,20 @@ def load_and_prep_photom(debug_folder):
 #     smrx_filename=soutput.spike2_file)
 
 
-# In[4]:
+# In[31]:
 
 
 data_list = []
 
 for d in debug_folders:
     data_dict = dict(df_pycontrol=None, pycontrol_time=None, data_photometry=None,
-                     photometry_times_pyc=None, photometry_keys=None, debug_folder=None)
+                    photometry_times_pyc=None, photometry_keys=None, debug_folder=None,
+                    sinput=None, soutput=None)
+    
     data_dict['df_pycontrol'], data_dict['pycontrol_time'], data_dict['data_photometry'], \
-        data_dict['photometry_times_pyc'], data_dict['photometry_keys'] = load_and_prep_photom(
-            d)
+        data_dict['photometry_times_pyc'], data_dict['photometry_keys'], \
+        data_dict['sinput'], data_dict['soutput'] = load_and_prep_photom(d)
+    
     data_dict['debug_folder'] = d
     data_list.append(data_dict)
 df_data = pd.DataFrame(data_list)
@@ -161,7 +171,7 @@ df_data['date_time'] = [r['date_time']
 # they join the windows with overlap somehow to recover data
 # 
 
-# In[5]:
+# In[13]:
 
 
 sampling_rate = df_data.loc[0,'data_photometry']['sampling_rate']
@@ -176,8 +186,16 @@ n_overlap = int(n_win_size/2)  # 50% overlap
 analog_1 = df_data.loc[0,'data_photometry']['analog_1']
 analog_2 = df_data.loc[0,'data_photometry']['analog_2']
 
+analog_1_filt = df_data.loc[0,'data_photometry']['analog_1_filt']
+analog_2_filt = df_data.loc[0,'data_photometry']['analog_2_filt']
 
-# In[6]:
+analog_1_est_motion = df_data.loc[0,'data_photometry']['analog_1_est_motion']
+analog_1_corrected = df_data.loc[0,'data_photometry']['analog_1_corrected']
+analog_1_baseline_fluo = df_data.loc[0,'data_photometry']['analog_1_baseline_fluo']
+analog_1_df_over_f = df_data.loc[0,'data_photometry']['analog_1_df_over_f']
+
+
+# In[14]:
 
 
 def overlapping_chunks(data1, data2, n_win_size: int, n_overlap: int):
@@ -205,7 +223,7 @@ start_index_chunks = []
 analog_1_est_motion_chunks = []
 p_vals = []
 r_vals = []
-for start_ind, chunk1, chunk2 in overlapping_chunks(analog_1, analog_2, 
+for start_ind, chunk1, chunk2 in overlapping_chunks(analog_1_filt, analog_2_filt, 
                                               n_win_size, n_overlap):
     slope, intercept, r_value, p_value, std_err = linregress(chunk2, chunk1)
 
@@ -228,16 +246,12 @@ for start_ind, chunk1, chunk2 in overlapping_chunks(analog_1, analog_2,
 # 
 # $$
 
-# In[7]:
+# In[15]:
 
 
 # reconstruct a vector
 
-analog_1_est_motion_joined = np.zeros(np.size(analog_1))
-
-
-# In[8]:
-
+analog_1_est_motion_joined = np.zeros(np.size(analog_1_filt))
 
 step_size = n_win_size - int(n_win_size) // int(n_win_size/n_overlap)
 
@@ -265,11 +279,100 @@ for i in range(0, len(start_index_chunks)):
         print('should not happen')
 
 
-# In[9]:
+# In[16]:
 
 
 fig = plt.figure()
 
 plt.plot(analog_1_est_motion_joined)
 plt.show()
+
+
+# In[25]:
+
+
+from scipy.signal import butter, filtfilt, medfilt
+
+analog_1_corrected_joined = analog_1_filt - analog_1_est_motion_joined
+low_pass_cutoff = 0.001
+b, a = butter(2, low_pass_cutoff, btype='low',
+              fs=sampling_rate)
+
+analog_1_df_over_f_joined = analog_1_corrected_joined / analog_1_baseline_fluo
+
+df_data.loc[0, 'data_photometry']['analog_1_est_motion_joined'] = analog_1_est_motion_joined
+df_data.loc[0, 'data_photometry']['analog_1_corrected_joined'] = analog_1_corrected_joined
+df_data.loc[0, 'data_photometry']['analog_1_df_over_f_joined'] = analog_1_df_over_f_joined
+
+
+# In[28]:
+
+
+fn = list(Path(df_data.loc[0,'sinput'].photometry_folder).glob('*.ppd'))
+# if fn == []:
+#     data_photometry = None    
+# else:
+#     fn = fn[0]
+#     data_photometry = import_ppd(fn)
+
+#     data_photometry = denoise_filter(data_photometry)
+#     data_photometry = motion_correction(data_photometry)
+#     data_photometry = compute_df_over_f(data_photometry, low_pass_cutoff=0.001)
+
+# %% Load data
+df_pycontrol = pd.read_pickle(df_data.loc[0,'sinput'].pycontrol_dataframe)
+
+pycontrol_time = df_pycontrol[df_pycontrol.name == 'rsync'].time
+
+# assuming just one txt file
+pycontrol_txt = list(
+    Path(df_data.loc[0, 'sinput'].pycontrol_folder).glob('*.txt'))
+
+with open(pycontrol_txt[0], 'r') as f:
+    all_lines = [line.strip() for line in f.readlines() if line.strip()]
+
+count = 0
+print_lines = []
+while count < len(all_lines):
+    # all_lines[count][0] == 'P'
+    if bool(match('P\s\d+\s', all_lines[count])):
+        print_lines.append(all_lines[count][2:])
+        count += 1
+        while (count < len(all_lines)) and not (bool(match('[PVD]\s\d+\s', all_lines[count]))):
+            print_lines[-1] = print_lines[-1] + \
+                "\n" + all_lines[count]
+            count += 1
+    else:
+        count += 1
+
+v_lines = [line[2:] for line in all_lines if line[0] == 'V']
+
+
+# %%
+if fn == []:
+    photometry_times_pyc = None
+else:
+    photometry_aligner = Rsync_aligner(
+        pycontrol_time, df_data.loc[0,'data_photometry']['pulse_times_2'])
+    photometry_times_pyc = photometry_aligner.B_to_A(df_data.loc[0,'data_photometry']['time'])
+
+# remove all state change event
+df_pycontrol = df_pycontrol.dropna(subset='name')
+df2plot = df_pycontrol[df_pycontrol.type == 'event']
+# state is handled separately with export_state, whereas parameters are handled vchange_to_text
+
+keys = df2plot.name.unique()
+
+photometry_keys = ['analog_1', 'analog_2',  'analog_1_filt', 'analog_2_filt',
+                   'analog_1_est_motion', 'analog_1_corrected', 'analog_1_baseline_fluo',
+                   'analog_1_df_over_f']
+
+# %%
+export_session(df_pycontrol, keys,
+               data_photometry=df_data.loc[0, 'data_photometry'],
+               photometry_times_pyc=photometry_times_pyc,
+               photometry_keys=photometry_keys,
+               print_lines=print_lines,
+               v_lines=v_lines,
+               smrx_filename=df_data.loc[0, 'soutput'].spike2_file)
 
