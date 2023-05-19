01178f9a328b866e1a992a4d829bfc6a8b7e6247
Teris Tam <teristam@gmail.com>
2023-05-19 18:11:38 +0100

    plotting

--- workflow/scripts/05_plot_pyphotometry.py
+++ workflow/scripts/05_plot_pyphotometry.py
@@ -40,20 +40,26 @@ sns.set_context('talk')
 
 for k in xr_session.data_vars.keys():
     da = xr_session[k]
-    if 'event_time' in da.coords:
+    if 'event_time' in da.coords: # choose data varialbes that are event related
         df2plot = xr_session[[k,'trial_outcome']].to_dataframe().reset_index()
+        trial_outcome = df2plot['trial_outcome'].unique()
         
-        if not all(df2plot[k].isna()): #make sure data are correct
+        for outcome in trial_outcome:
+          df_outcome = df2plot[df2plot['trial_outcome']==outcome]
           
-          # fig, ax = plt.subplots(1,1,dpi=300, figsize=(6,6))
+          if not all(df_outcome[k].isna()): #make sure data are correct
+            
+            fig, ax = plt.subplots(1,1,dpi=300, figsize=(6,6))
 
-          g = sns.relplot(x='event_time',col='trial_outcome', y=k, 
-                           hue ='trial_outcome',
-                           kind='line',data=df2plot)
-          g.set_xlabels('Time (ms)')
-          g.set_ylabels(k)
-          
-          g.figure.savefig(os.path.join(figure_dir, f'{k}.png'), dpi=300, bbox_inches='tight')
+            sns.lineplot(x='event_time',y=k, 
+                            hue ='trial_outcome',
+                            data=df_outcome)
+            ax.set_xlabel('Time (ms)')
+            ax.set_ylabel(k)
+            ax.set_title(outcome)
+            
+            fig.savefig(os.path.join(figure_dir, f'{k}_{outcome}.png'), dpi=300, bbox_inches='tight')
+            # g.figure.savefig(os.path.join(figure_dir, f'{k}.png'), dpi=300, bbox_inches='tight')
 
 xr_session.close()
 
