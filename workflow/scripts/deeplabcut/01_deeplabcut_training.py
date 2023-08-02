12c8c7f6faab6a69ab5bf71962d16282a64e13a2
Teris <teristam@gmail.com>
2023-08-02 15:29:45 +0100

    add create dataset

--- workflow/scripts/deeplabcut/01_deeplabcut_training.py
+++ workflow/scripts/deeplabcut/01_deeplabcut_training.py
@@ -1,5 +1,11 @@
 #%%
 import deeplabcut
+
+#%% create training dataset
+path_config_file = 'Z:/Teris/ASAP/deeplabcut/side_2_hands_newobj-julien-2022-08-26/config.yaml'
+
+deeplabcut.create_training_dataset(path_config_file)
+
 #%%
-path_config_file = '/home/MRC.OX.AC.UK/ndcn1330/ettin/Teris/ASAP/deeplabcut/side_2_hands_newobj-julien-2022-08-26/config.yaml'
-deeplabcut.train_network(path_config_file, gputouse=0, saveiters=300, displayiters=100, maxiters=200000)
+deeplabcut.train_network(path_config_file, gputouse=0, saveiters=300, 
+                         displayiters=100, maxiters=200000)
