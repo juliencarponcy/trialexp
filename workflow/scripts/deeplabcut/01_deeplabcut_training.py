26eb9b736ed377533ac1e744e4a75d39ad7994e1
Teris Tam <teristam@gmail.com>
2023-08-04 14:28:10 +0100

    add load dot file

--- workflow/scripts/deeplabcut/01_deeplabcut_training.py
+++ workflow/scripts/deeplabcut/01_deeplabcut_training.py
@@ -1,10 +1,13 @@
 #%%
 import deeplabcut
-
+import os
+import dotenv
 #%% create training dataset
-path_config_file = 'Z:/Teris/ASAP/deeplabcut/side_2_hands_newobj-julien-2022-08-26/config.yaml'
+dotenv.load_dotenv()
 
-deeplabcut.create_training_dataset(path_config_file)
+path_config_file = os.environ['ETTIN_MOUNT_PATH']+'/Teris/ASAP/deeplabcut/side_2_hands_newobj-julien-2022-08-26/config.yaml'
+
+# deeplabcut.create_training_dataset(path_config_file)
 
 #%%
 deeplabcut.train_network(path_config_file, gputouse=0, saveiters=300, 
