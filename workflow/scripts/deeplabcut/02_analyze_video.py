61a528b2703c7516fe394ed8a7c58b110182e5a0
Teris Tam <teristam@gmail.com>
2023-08-04 14:28:26 +0100

    clean up

--- workflow/scripts/deeplabcut/02_analyze_video.py
+++ workflow/scripts/deeplabcut/02_analyze_video.py
@@ -23,14 +23,16 @@ side_cam = [str(video_path/(f+'.mp4')) for f in filelist if 'Side' in f]
 dlc_result = Path(soutput.dlc_result)
 
 #%%
-# video = deeplabcut.DownSampleVideo(side_cam[0], width=640, outpath=str(dlc_result.parents[1]/'video'))
 outpath=str(dlc_result.parents[1]/'video'/'side_downsampled.mp4')
-video = dlc_utils.rescale_video(side_cam[0], output_path=outpath, width=640, frame_rate=100)
+if not Path(outpath).exists():
+    video = dlc_utils.rescale_video(side_cam[0], output_path=outpath, width=640, frame_rate=100)
 
 # %% analyze video
-deeplabcut.analyze_videos(path_config_file, [video], gputouse=0, batchsize=16,
+deeplabcut.analyze_videos(path_config_file, [outpath], gputouse=0, batchsize=64,
                           destfolder=dlc_result.parent)
 
 # %% rename the DLC results to better work with snakemake
 dlc_file = glob.glob(str(dlc_result.parent/'*.h5'))[0]
 shutil.copy(dlc_file, soutput.dlc_result)
+
+# %%
