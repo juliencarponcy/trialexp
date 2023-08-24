3721c46972b7897a20a7540f7122ec35474d3d27
Teris Tam <teristam@gmail.com>
2023-08-24 17:01:43 +0100

    bugfix: previous sync direction is wrong doc: more explanation about the processing

--- workflow/scripts/spike_sorting/s04_ephys_sync.py
+++ workflow/scripts/spike_sorting/s04_ephys_sync.py
@@ -40,6 +40,8 @@ sync_path = kilosort_folder.parents[1]/'ephys'
 
 for probe_dir in  kilosort_folder.glob('Probe*'):
     # change the spike timing based on the rsync signal from both pycontrol and openephys
+    # event time from open ephys count from the beginning of the acquisition, not recording
+    # kilosort time always start from the beginning of the recording
 
     rec_prop = pd.read_csv(probe_dir/'rec_prop.csv').iloc[0]
     rsync = create_ephys_rsync(str(pycontrol_path), sync_path, rec_prop.tstart)
@@ -55,10 +57,16 @@ for probe_dir in  kilosort_folder.glob('Probe*'):
 
     # Careful with the forced 30 value, sampling rate slightly diverging from 30kHz
     # should be dealt-with by open-ephys/ks3, but need to be checked
+    # Note: anything that happen before the first ephys rsync pulse and after the last ephys rsync
+    # pulse will not be synchronized and result in nan
+    # Will keep the nan in synced_spike_times to be consistent with CellExplorer spike count
     spike_times_converted = spike_times/(rec_prop.sample_rate/1000) # /1000 to be in ms
-    synced_spike_times = rsync.B_to_A(spike_times_converted)
+    synced_spike_times = rsync.A_to_B(spike_times_converted) #convert ephys to pycontrol time
+    
+    # do some sanity check to make sure we are syncing the correct file
+    # Unlikely that there is no spike for 1 minute
+    lastspike2end = abs(np.nanmax(synced_spike_times)/1000 - rec_prop.duration)
+    assert lastspike2end<60, f'Error: last spike appears too far away from end of recording {lastspike2end}. Potential sync issues'
 
     np.save(ks3_path.parent / 'rsync_corrected_spike_times.npy', synced_spike_times)
-    
 
-# %%
